960e6af70113756f01f4c2f9d820abfe20665334
MelinaClouet <melina.clouet@my-digital-school.org>
2026-04-13 17:58:35 +0200

    [ADD] Create new page views and routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
+});
+
+Route::get('/projets', function () {
+    return view('pages.projects');
+});
+
+Route::get('/a-propos', function () {
+    return view('pages.about');
+});
+
+Route::get('/contact', function () {
+    return view('pages.contact');
 });
