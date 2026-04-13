24367e3bf4176a12037878b3796aaa6449e559c2
MelinaClouet <melina.clouet@my-digital-school.org>
2026-04-13 17:32:40 +0200

    FIRST COMMIT

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Portfolio</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-50">
+
+<div class="min-h-screen flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-indigo-600">
+        Mon Portfolio 🚀
+    </h1>
+</div>
+
+</body>
+</html>
